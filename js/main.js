d738f2564a4333ce8f8d0e212d489540662dee24
Jake Lawrence <jake.lawrence24@gmail.com>
2017-11-22 18:04:58 -0500

    some fade added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
     $('#code-a').hover(function() {
         $('.site-head-middle').css('border-bottom', '11px solid #EF5350');
+        $('.site-head-middle').css('-webkit-transition', 'border-bottom 1s ease');
     }, function() {
         $('.site-head-middle').css('border-bottom', '11px solid white');
     });
 
     $('#resume').hover(function() {
         $('.site-head-middle').css('border-bottom', '11px solid #FFA726');
+        $('.site-head-middle').css('-webkit-transition', 'border-bottom 1s ease');
     }, function() {
         $('.site-head-middle').css('border-bottom', '11px solid white');
     });
 
     $('#prof').hover(function() {
         $('.site-head-middle').css('border-bottom', '11px solid #FFD600');
+        $('.site-head-middle').css('-webkit-transition', 'border-bottom 1s ease');
     }, function() {
         $('.site-head-middle').css('border-bottom', '11px solid white');
     });
 
     $('#logos').hover(function() {
         $('.site-head-middle').css('border-bottom', '11px solid #00E676');
+        $('.site-head-middle').css('-webkit-transition', 'border-bottom 1s ease');
     }, function() {
         $('.site-head-middle').css('border-bottom', '11px solid white');
     });
 
     $('#contact').hover(function() {
         $('.site-head-middle').css('border-bottom', '11px solid #00E5FF');
+        $('.site-head-middle').css('-webkit-transition', 'border-bottom 1s ease');
     }, function() {
         $('.site-head-middle').css('border-bottom', '11px solid white');
     });
-
-    var current_color = 0;
-    var colors = ['#EF5350', '#FFA726', '#FFD600', '#00E676', '#00E5FF',
-                  '#5C6BC0', '#7E57C2'];
 });
